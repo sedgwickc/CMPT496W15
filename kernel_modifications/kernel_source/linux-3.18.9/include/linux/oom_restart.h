be2c624644e015b890b0bef711bf4a709cea3bad
Charles Sedgwick <sedgwickc@mymacewan.ca>
2015-03-14 23:44:16 -0600

    Add kernel source files and dmesg output that shows oom_restart output.

---
+++ kernel_modifications/kernel_source/linux-3.18.9/include/linux/oom_restart.h
@@ -0,0 +1,16 @@
+#ifndef __INCLUDE_LINUX_OOM_RESTART_H
+#define __INCLUDE_LINUX_OOM_RESTART_H
+/* Charles Sedgwick
+ * CMPT496W15
+ * This file contains the functions that allow the kernel to restart a process
+ * that it has killed due to a oom situation. 
+ * 
+ * A process that has been killed is eligible for restart if it has been running
+ * for minimum amount of time. This is to prevent the kernel getting stuck in a
+ * loop where it constantly restarts a process that quickly causes an oom
+ * situiation. 
+ */
+
+extern void oom_restart(void);
+
+#endif
