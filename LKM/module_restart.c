ed894363618a1a6a0a43db34f4ec38205792f398
Charles Sedgwick <sedgwickc@mymacewan.ca>
2015-04-14 16:07:44 -0600

    Netlink connection between LKM and restarter task now works.
    The user task sends a message to the LKM. The LKM saves the pid and uses it to
    send the cmdline if an oom situation occurs.

--- LKM/module_restart.c
+++ LKM/module_restart.c
@@ -1,148 +1,115 @@
-/* Charles Sedgwick
- */
-#include <linux/module.h> /* needed by all modules */
-#include <linux/kernel.h> /* needed for KERN_INFO */
-#include <linux/init.h> /* needed for macros */
-#include <linux/moduleparam.h> /* needed for macros */
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <net/sock.h>
+#include <linux/socket.h>
+#include <linux/net.h>
+#include <asm/types.h>
+#include <linux/netlink.h>
+#include <linux/rtnetlink.h>
+#include <linux/skbuff.h>
+#include <linux/delay.h>
 #include <linux/oom_restart.h>
-#include <net/genetlink.h> /* needed for netlink */
-#include <net/netlink.h>
 
 #define DRIVER_AUTHOR "Charles Sedgwick <sedgwickcharles@gmail.com>"
 #define DRIVER_DESC "A test module"
-
-#define GROWTH_THRESH 5000
-#define RESTART_A_MAX (__RESTART_A_MAX - 1)
-#define RESTART_C_MAX (__RESTART_C_MAX - 1)
-#define RESTART_GRP "RESTART_GRP" /* user defined group for multicast */
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 MODULE_SUPPORTED_DEVICE("test_device");
 
-enum {
-	RESTART_A_UNSPEC,
-	RESTART_A_MSG,
-	__RESTART_A_MAX,
-};
+#define NETLINK_RESTART 30
+#define MYGRP 25 //User defined group, consistent in both kernel prog and user prog
 
-enum {
-	RESTART_C_UNSPEC,
-	RESTART_C_CMD_SEND,
-	__RESTART_C_MAX,
-};
+struct sock *nl_sk = NULL;
+int user_pid = 0;
 
-/* Family Definition */
-struct genl_family restart_task_genl_family = {
-	.id = GENL_ID_GENERATE,
-	.hdrsize = 0,
-	.name = "RESTART",
-	.version = 1,
-	.maxattr = RESTART_A_MAX,
-};
+static int cmd_send(void) {
+    struct sk_buff *skb_out;
+    struct nlmsghdr *nlh;
+    int msg_size;
+    char *msg = "Here is a cmdline from kernel";
+    int res;
 
-/* cmd line args 
-int cmd_line_arg = 0;
-module_param(cmd_line_arg, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-MODULE_PARM_DESC( cmd_line_arg, "An integer" ); 
-*/
+    printk(KERN_INFO "Entering: %s\n", __FUNCTION__);
+    msg_size = strlen(msg);
+    skb_out = nlmsg_new(msg_size, 0);
 
-/*
- * restart_genl_notify_dump
- * currently a stub that does nothing
- * could be used to send sent cmds back on dump request
- */
-int restart_genl_notify_dump( struct sk_buff *skb, struct netlink_callback *cb )
-{
-	return 0;
+    if (!skb_out) {
+        printk(KERN_ERR "Failed to allocate new skb\n");
+        return -1;
+    }
+    nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
+    NETLINK_CB(skb_out).dst_group = 0; /* not in multicast group */
+    strncpy(nlmsg_data(nlh), msg, msg_size);
+
+    res = nlmsg_unicast(nl_sk, skb_out, user_pid);
+    if (res < 0) {
+        printk(KERN_INFO "Error while sending to user space. Error id: %d\n", res);
+        return res;
+    }
+
+    return 0;
 }
 
-int cmd_send(void)
+static void restart_register_pid( struct sk_buff *skb )
 {
-	struct sk_buff *skb_out = NULL;
-	void *msg_head;
-	int rc;
 	
-	skb_out = genlmsg_new( NLMSG_GOODSIZE, GFP_KERNEL );
-	if( skb_out == NULL )
-		return -ENOMEM;
-	msg_head = genlmsg_put( skb_out, 0, 0, &restart_task_genl_family, 0, 
-		RESTART_C_CMD_SEND );
-	if( msg_head == NULL )
-	{
-		return -ENOMEM;
-	}
+    struct sk_buff *skb_out;
+    struct nlmsghdr *nlh;
+    int msg_size;
+    char *msg = "Hello from kernel";
+    int res;
 
-	rc = nla_put_string( skb_out, RESTART_A_MSG, "Hello from kernel space!" );
-	if ( rc != 0 )
-		return rc;
+    printk(KERN_INFO "Entering: %s\n", __FUNCTION__);
+    msg_size = strlen(msg);
+    nlh = (struct nlmsghdr *)skb->data;
+    printk(KERN_INFO "The kernel received the following payload: %s\n",
+    	(char*)nlmsg_data(nlh));
+   	user_pid = nlh->nlmsg_pid; /* pid of sending process */
 
-	genlmsg_end( skb_out, msg_head );
-	rc = genlmsg_unicast( &init_net, skb_out, 7114); 
-//	rc = genlmsg_multicast( &restart_task_genl_family, skb_out, 0, 0, 0	);
-	if ( rc != 0 )
-		return rc;
+    skb_out = nlmsg_new(msg_size, 0);
 
-	return 0;
+    if (!skb_out) {
+        printk(KERN_ERR "Failed to allocate new skb\n");
+    }
+    nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
+    NETLINK_CB(skb_out).dst_group = 0; /* not in multicast group */
+    strncpy(nlmsg_data(nlh), msg, msg_size);
+
+    res = nlmsg_unicast(nl_sk, skb_out, user_pid);
+
+    if (res < 0) {
+        printk(KERN_INFO "Error while sending to user space. Error id: %d\n", res);
+    }
+
 }
 
-int restart_cb( struct notifier_block *nb, unsigned long code, void *_param )
-{
-	cmd_send();
-	return 0;
-}
+static int __init restart_task_init(void) {
 
-/* init function */
-int __init restart_task_init(void)
-{
-	int rc;
-	
-	struct nla_policy restart_task_genl_policy[ RESTART_A_MAX + 1] = {
+    struct netlink_kernel_cfg cfg = {
+    	.input = restart_register_pid,
+            //.groups = MYGRP,
+    };
+    printk(KERN_INFO "Starting restart_task module");
+    nl_sk = netlink_kernel_create(&init_net, NETLINK_RESTART, &cfg);
+    if (!nl_sk) {
+        printk(KERN_ALERT "Error creating socket.\n");
+        return -10;
+    }
 
-		[RESTART_A_MSG] = { .type = NLA_NUL_STRING },
-	};
-
-	struct genl_ops ops[] = {
-		{
-			.cmd = RESTART_C_CMD_SEND,
-			.flags = 0,
-			.policy = restart_task_genl_policy,
-			.dumpit = restart_genl_notify_dump,
-			.doit = NULL,
-		},
-	};
-			
-	struct genl_multicast_group grps[] = {
-		{
-			.name = RESTART_GRP,
-		},
-	};
-
-	printk( KERN_INFO "OOM_RESTART: initializing\n" );
-	
 	/* set function to be called by kernel to send cmdline */
 	set_cmd_func( cmd_send );
 
-
-
-	/* use register with ops and groups */
-//	rc = genl_register_family_with_ops_groups( &restart_task_genl_family, ops, grps );
-//	if( rc != 0 )
-//	{
-//		pr_err( "OOM_RESTART: Unable to register netlink family" );
-//		return rc;
-//	}
-
-	/* a non 0 return means the module cannot be loaded */
-	return 0;
+    return 0;
 }
 
-void __exit restart_task_cleanup(void)
-{
-	genl_unregister_family( &restart_task_genl_family );
-	printk( KERN_INFO "OOM_RESTART: Unloading... \n" );
+static void __exit restart_task_exit(void) {
+
+    printk(KERN_INFO "Exiting restart module\n");
+    netlink_kernel_release(nl_sk);
 }
 
 module_init(restart_task_init);
-module_exit(restart_task_cleanup);
+module_exit(restart_task_exit);
